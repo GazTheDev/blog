7d8719e65185f1ec2fbc544cbadeed5d98dbb060
GazTheDev <36386036+GazTheDev@users.noreply.github.com>
2018-04-13 15:26:45 +0100

    initial commit

---
+++ src/pages/13-04-2018-post-one/index.md
@@ -0,0 +1,11 @@
+---
+
+path: "/post-one"
+date: "13/04/2018"
+title: "Welcome" 
+author: "Gareth"
+
+---
+
+This is my very first blog post in gatsby JS
+
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <div>
+        <h1>About Us</h1>
+        <p>Welcome to our website.
+            </p>
+    </div>
+)
+
+export default AboutPage;
